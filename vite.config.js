d3f1d5a742226f9899146a832549f8565d765ee7
Jonathan Sanhueza <jonathan.sanhueza.c@gmail.com>
2023-05-07 02:54:24 -0400

    mas correcciones

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["firebase/firestore"],
+      external: [
+        "firebase/firestore",
+        "firebase/app",
+      ],
     },
   },
 });
